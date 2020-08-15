0c795420379a3cd071d330aeb2994f14104ac546
Gaurav Naresh Pandit <gaurav@rapidalley.com>
2020-08-15 09:43:22 +0530

    Google Maps Current Location Added

---
+++ app/src/main/java/com/example/newsapp/model/location/LocationService.java
@@ -0,0 +1,218 @@
+package com.example.newsapp.model.location;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class LocationService extends Service implements LocationListener {
+
+
+    private Context mContext;
+    boolean checkGPS = false;
+    private String TAG = "location_service";
+    boolean checkNetwork = false;
+
+    boolean canGetLocation = false;
+
+    Location loc;
+    double latitude;
+    double longitude;
+
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+
+
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    protected LocationManager locationManager;
+
+    private String country_name;
+
+    public LocationService(Context context){
+        this.mContext = context;
+        getLocation();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+
+    private Location getLocation() {
+
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // get GPS status
+            checkGPS = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // get network provider status
+            checkNetwork = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!checkGPS && !checkNetwork) {
+                Toast.makeText(mContext, "No Service Provider is available", Toast.LENGTH_SHORT).show();
+            } else {
+                this.canGetLocation = true;
+
+                // if GPS Enabled get lat/long using GPS Services
+                if (checkGPS) {
+
+                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION);
+                    }// TODO: Consider calling
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        loc = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (loc != null) {
+                            latitude = loc.getLatitude();
+                            longitude = loc.getLongitude();
+                        }
+//                        Geocoder geocoder = new Geocoder(getApplicationContext());
+//                        try {
+//                            List<Address> addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+//                            if(addresses != null && addresses.size() > 0) {
+//                                country_name = addresses.get(1).getCountryName();
+//                                Log.d(TAG, "getLocation: "+country_name);
+//                                Toast.makeText(getApplicationContext(), country_name, Toast.LENGTH_LONG).show();
+//                            }
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                            Log.d(TAG, "getLocation: "+e.toString());
+//                        }
+                    }
+
+
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return loc;
+    }
+
+    public String getCountryName(Context context, double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            Address result;
+
+            if (addresses != null && !addresses.isEmpty()) {
+                return addresses.get(0).getCountryName();
+            }
+            return null;
+        } catch (IOException ignored) {
+            //do something
+        }
+        return null;
+    }
+
+
+    public double getLongitude() {
+        if (loc != null) {
+            longitude = loc.getLongitude();
+        }
+        return longitude;
+    }
+
+    public double getLatitude() {
+        if (loc != null) {
+            latitude = loc.getLatitude();
+        }
+        return latitude;
+    }
+
+    public String getCountry_name(){
+        return country_name;
+    }
+
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+
+        alertDialog.setTitle("GPS is not Enabled!");
+
+        alertDialog.setMessage("Do you want to turn on GPS?");
+
+
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        alertDialog.show();
+    }
+
+
+    public void stopListener() {
+        if (locationManager != null) {
+
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.removeUpdates(LocationService.this);
+        }
+    }
+
+}
--- app/src/main/java/com/example/newsapp/view/MainActivity.java
+++ app/src/main/java/com/example/newsapp/view/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.newsapp.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.newsapp.R;
 import com.example.newsapp.model.adapter.NewsAdapter;
 import com.example.newsapp.model.constants.Constants;
+import com.example.newsapp.model.location.LocationService;
 import com.example.newsapp.model.pojoclass.Article;
 import com.example.newsapp.model.pojoclass.NewsAPIResponse;
 import com.example.newsapp.viewModel.MainViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     private long totalResults;
     private NewsAdapter newsAdapter;
     private RecyclerView newsRecyclerView;
+    private String country_name = "in";
+
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+    LocationService locationService;
 
     public MainActivity() {
     }
@@ -37,18 +53,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        country = "us";
+
+        checkLocationPermission();
         category = "business";
         apiKey = Constants.apiKey;
 
         newsRecyclerView = findViewById(R.id.news_recycler_view);
         newsRecyclerView.setHasFixedSize(true);
-        Log.d(TAG, "Country: "+country);
         Log.d(TAG, "Category: "+category);
         Log.d(TAG, "apiKey: "+apiKey);
         mMainViewModel = new MainViewModel();
-        mMainViewModel.getCountrySpecificNews(country, category, apiKey);
-        observeViewModel();
+
+        locationService = new LocationService(MainActivity.this);
+        if (locationService.canGetLocation()) {
+            double longitude = locationService.getLongitude();
+            double latitude = locationService.getLatitude();
+            country_name = locationService.getCountryName(this, latitude, longitude);
+            if ("India".equals(country_name)){
+                country_name = "in";
+            }else if ("United States".equals(country_name)){
+                country_name = "us";
+            }
+            Log.d(TAG, "Country: "+country_name);
+            mMainViewModel.getCountrySpecificNews(country_name, category, apiKey);
+            observeViewModel();
+        } else {
+            locationService.showSettingsAlert();
+        }
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationService = new LocationService(MainActivity.this);
+        if (locationService.canGetLocation()) {
+            double longitude = locationService.getLongitude();
+            double latitude = locationService.getLatitude();
+            country_name = locationService.getCountryName(this, latitude, longitude);
+            if ("India".equals(country_name)){
+                country_name = "in";
+            }else if ("United States".equals(country_name)){
+                country_name = "us";
+            }
+            Log.d(TAG, "Country: "+country_name);
+            mMainViewModel.getCountrySpecificNews(country_name, category, apiKey);
+            observeViewModel();
+        } else {
+            locationService.showSettingsAlert();
+        }
     }
 
     private void observeViewModel() {
@@ -71,4 +126,65 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ALL_PERMISSIONS_RESULT);
+
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ALL_PERMISSIONS_RESULT);
+            }
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == ALL_PERMISSIONS_RESULT) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this,
+                        ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    //Request location updates:
+                    locationService = new LocationService(MainActivity.this);
+                    if (locationService.canGetLocation()) {
+                        double longitude = locationService.getLongitude();
+                        double latitude = locationService.getLatitude();
+                        Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+                    } else {
+                        locationService.showSettingsAlert();
+                    }
+
+                }
+
+            }
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationService.stopListener();
+    }
+
+
 }
